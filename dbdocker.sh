5776b3a26f2c4a7df12472281baaa1192edb7f6f
hyperion4040 <hyperion1987@wp.pl>
2017-05-23 10:45:28 +0200

    refactor of dbdocker script

--- dbdocker.sh
+++ dbdocker.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 if [  "$1" == kill ]; then
-echo "Container of Oracle DB with id = $(sudo docker stop $(sudo docker ps -q)) was stopped"
-if [ "$(sudo docker ps -aq -f status=exited -f status=created)" ]; then
-echo "Container of Oracle DB with id = $(sudo docker rm $(sudo docker ps -q -f status=exited -f status=created)) was removed"
+echo "Container of DB with id = $(docker stop $(docker ps -q)) was stopped"
+if [ "$(docker ps -aq -f status=exited -f status=created)" ]; then
+echo "Container of DB with id = $(docker rm $(docker ps -q -f status=exited -f status=created)) was removed"
 fi
 else
-echo "Container of Oracle DB with id = $(sudo docker run -d -p 49160:22 -p 49161:1521 -p 49162:8080 wnameless/oracle-xe-11g) had been created and is running"
+case $1 in 
+oracle)
+echo "Container of Oracle DB with id = $(docker run -d -p 49160:22 -p 49161:1521 -p 49162:8080 wnameless/oracle-xe-11g) had been created and is running";;
+mysql) 
+echo "Constainer of MySQL DB with id = $(docker run -d -t -p 3306:3306 -e MYSQL_ROOT_PASSWORD=rootpass mysql) had been created and is running";;
+*)
+echo "Actually you can choose Oracle or MySQL DB and run in container"
+exit 1
+esac
 fi
+
 
--- volumeOnStart.sh
+++ volumeOnStart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # This is bash command which is executed from /etc/rc.local. 
-/usr/bin/pactl set-sink-volume 1 25%
+# /usr/bin/pactl set-sink-volume 1 5%
+return 0
