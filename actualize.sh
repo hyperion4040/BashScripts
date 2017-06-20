850abf61302cc6907316708f8c5c39af297f6ef9
hyperion4040 <hyperion1987@wp.pl>
2017-06-20 18:42:25 +0200

    new if statement in actualize.sh

--- actualize.sh
+++ actualize.sh
@@ -2,6 +2,8 @@ if [ -z "$1" ]; then
 	sudo apt-get update
 elif [ "$1" == now ]; then 
 	sudo apt-get update && sudo apt-get upgrade
+elif [ "$1" == all ]; then
+	sudo apt-get update && sudo apt-get dist-upgrade
 elif [ "$1" == shutdown ]; then
 	sudo apt-get update && sudo apt-get upgrade && shutdown -h now
 	
