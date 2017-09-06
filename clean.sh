ed62cab094b78f1c2fd5529fce4b0c79113ae959
hyperion4040 <hyperion1987@wp.pl>
2017-09-06 17:48:52 +0200

    change of clean.sh file

--- clean.sh
+++ clean.sh
@@ -3,7 +3,9 @@
 if [ -z "$1" ]; then
 		sudo apt-get clean
 	elif [ "$1" == rm ]; then
-		sudo apt-get autoremove 
+		sudo apt-get autoremove
+	elif [ "$1" == all ]; then
+	    ubuntu-cleaner
 	else
 		echo "wrong command"
 fi
