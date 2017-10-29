33e69403f04fe75ea308d378775ce6ed561d227e
hyperion4040 <hyperion1987@wp.pl>
2017-10-29 13:40:59 +0100

    new version of dbdocker.sh

--- dbdocker.sh
+++ dbdocker.sh
@@ -10,6 +10,11 @@ oracle)
 echo "Container of Oracle DB with id = $(docker run -d -p 49160:22 -p 49161:1521 -p 49162:8080 wnameless/oracle-xe-11g) had been created and is running";;
 mysql) 
 echo "Constainer of MySQL DB with id = $(docker run -d -t -p 3306:3306 -e MYSQL_ROOT_PASSWORD=rootpass mysql) had been created and is running";;
+postgres)
+echo "Container of postgres DB with id = $(docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres) had been created and is running";;
+psql) 
+echo "Container of postgres DB with id = $(docker run --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres) had been created and is running";;
+echo "Acces do postgres by bash $(docker run -it --rm --link some-postgres:postgres postgres psql -h postgres -U postgres)"
 *)
 echo "Actually you can choose Oracle or MySQL DB and run in container"
 exit 1
